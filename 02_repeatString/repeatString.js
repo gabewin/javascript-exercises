eac8ebe1fb14944e7b132873078956eb358ea989
gabewin <gabewin@protonmail.com>
2022-08-08 12:00:06 -0700

    js exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,repeat) {
+    let response = '';
+    if (repeat<0){
+        return "ERROR";
+    }
+    for (let i=0; i<repeat; i++){
+        response+=word;
+    }
+    return response;
 };
 
 // Do not edit below this line
