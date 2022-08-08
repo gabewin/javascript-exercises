83c7c8ad2fd0cd9f87ff27a29f1f6150439e1bff
gabewin <gabewin@protonmail.com>
2022-08-08 12:12:09 -0700

    js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverseWord = '';
 
+    for (let i=0; i<word.length; i++){
+        reverseWord = word[i]+reverseWord;
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
