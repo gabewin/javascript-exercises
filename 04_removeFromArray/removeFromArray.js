744bb25b6dd1ca210497437c4ca7411fda40169f
gabewin <gabewin@protonmail.com>
2022-08-09 06:32:15 -0700

    js exercises 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    console.log(args);
+    for (let i=array.length-1; i>=0; i--){
+        for (let a=0; a<args.length; a++){
+            console.log(`array[a] ${array[i]} args[i] ${args[a]}`)
+            if (array[i] === args[a]){
+                array.splice(i, 1);
+                
+            }
 
+        }
+    }
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
