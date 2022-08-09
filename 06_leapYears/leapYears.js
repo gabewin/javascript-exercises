90e3e882d7fae1909651481b8f0785818b201456
gabewin <gabewin@protonmail.com>
2022-08-09 11:23:46 -0700

    js exercises 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    let divBy4 = (year%4 == 0);
+    let divBy100 = (year%100 == 0);
+    let divBy400 = (year%400 ==0);
+    
+    if (divBy4){
+        if (divBy100){
+            if (divBy400){
+                return true;
+            }else{
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
