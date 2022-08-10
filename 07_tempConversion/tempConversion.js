0bdf4af9d4ca96e7cd58992a1f52dc5888cf4455
gabewin <gabewin@protonmail.com>
2022-08-10 05:53:23 -0700

    js exercises 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) {
+  tempc = ((tempf - 32)*(5/9));
+  return (Math.round(tempc *10)/10);
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  tempf = ((tempc *(9/5)+32));
+  return (Math.round(tempf *10)/10);
 };
+
+
 
 // Do not edit below this line
 module.exports = {
