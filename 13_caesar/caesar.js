c6d7d19fad99fb7212896d92c1d4de208749e525
gabewin <gabewin@protonmail.com>
2022-08-10 07:59:09 -0700

    start of js exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,12 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    let stringArray = string.split("");
+    const newStringArray = stringArray.map(x => String.fromCharCode(x.charCodeAt() + shift));
+    let newString= newStringArray.join("");
+
+    return newString;
 
 };
+
 
 // Do not edit below this line
 module.exports = caesar;
