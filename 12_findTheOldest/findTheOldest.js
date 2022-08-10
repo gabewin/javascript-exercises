c8e96a5efe28cb65a1398582ce0f135ce1a17a32
gabewin <gabewin@protonmail.com>
2022-08-10 07:36:23 -0700

    js exercises 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let total =0;
+    let OldestPerson = {};
+    let today = new Date();
 
+    for (let i = 0; i<people.length; i++){
+        if(!('yearOfDeath' in people[i])){
+            people[i].yearOfDeath = today.getFullYear();
+            console.log(people[i]);
+        }
+    }
+
+    people.reduce((prev, curr) => {
+        //console.log(curr.name);
+        console.log(prev.name);
+        if ((prev.yearOfDeath - prev.yearOfBirth)> total){
+            total = (prev.yearOfDeath - prev.yearOfBirth);
+            OldestPerson = prev;
+        }
+        if((curr.yearOfDeath - curr.yearOfBirth)> total){
+            total = (curr.yearOfDeath - curr.yearOfBirth);
+            OldestPerson = curr;
+        }
+        return prev;
+    })
+    return OldestPerson;
+ 
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.spec.js
+++ 12_findTheOldest/findTheOldest.spec.js
@@ -9,19 +9,19 @@ describe('findTheOldest', () => {
         yearOfDeath: 1970,
       },
       {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-      },
-      {
         name: "Jane",
         yearOfBirth: 1912,
         yearOfDeath: 1941,
       },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the oldest person if someone is still living', () => {
+  test('finds the oldest person if someone is still living', () => {
     const people = [
       {
         name: "Carly",
@@ -40,7 +40,7 @@ describe('findTheOldest', () => {
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the oldest person if the OLDEST is still living', () => {
+  test('finds the oldest person if the OLDEST is still living', () => {
     const people = [
       {
         name: "Carly",
