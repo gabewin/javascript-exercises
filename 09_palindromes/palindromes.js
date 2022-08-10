d5b5277970c50c92697475a25b5e290ff0749830
gabewin <gabewin@protonmail.com>
2022-08-10 06:29:13 -0700

    js exercises 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    reverseWord = "";
+    newWord = "";
+    for (let i=0; i<word.length; i++){
+        if (word[i].match(/[a-z]/i)){
+            reverseWord = word[i]+reverseWord;
+            newWord = newWord + word[i];
+        }
+    }
+    return reverseWord == newWord;
 };
 
 // Do not edit below this line
