3965a639f1190bf286c6b5b60445437383e6fddb
gabewin <gabewin@protonmail.com>
2022-08-10 06:11:21 -0700

    js exercises 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+	for (let i=0; i<array.length; i++){
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+	for (let i=0; i<array.length; i++){
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  if(number == 0){
+    return result;
+  } else {
+    for (let i = number; i> 0; i--){
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
