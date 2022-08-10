cbcc503370c6e0617e43c142e3436f160ba5f34a
gabewin <gabewin@protonmail.com>
2022-08-10 06:40:26 -0700

    javascript 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0){
+        return 'OOPS';
+    }
+    else if (n <= 1){
+        return Number(n);
+    } else {
+        return fibonacci(n-1) + fibonacci(n - 2);
+        
+    }
 };
 
 // Do not edit below this line
